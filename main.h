7d3f8781c57b0cbb4d72319b7cf55ea9c064e30d
bmwangangi <trabbart8@gmail.com>
2023-11-13 11:53:58 -0800

    added a file

--- main.h
+++ main.h
@@ -5,8 +5,6 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void print_reverse(const char *str);
-void custom_printf(const char *format, ...);
 int _printf(const char *format, ...);
 int custom_printf(const char *style, ...);
 int print_integer(int p);
