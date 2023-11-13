a2bbd0e44eab0f47ea148c5303dcc998e068615f
bmwangangi <trabbart8@gmail.com>
2023-11-13 11:51:21 -0800

    added a file

--- main.h
+++ main.h
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <stdio.h>
 
+void print_reverse(const char *str);
+void custom_printf(const char *format, ...);
 int _printf(const char *format, ...);
 int print_integer(va_list argument);
 int print_decimal(va_list argument);
