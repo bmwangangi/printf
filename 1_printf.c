8b9fff33fa74a4218647bbf02cf0c43f5c85ae20
bmwangangi <trabbart8@gmail.com>
2023-11-13 04:03:05 -0800

    edited the file 1_printf.c

--- 1_printf.c
+++ 1_printf.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * _printf - Custom printf-like function with variable arguments.
- * @format: Format string specifying the output format.
+ * _printf - custom like printf function.
+ * @format: specifying the output format.
  *
- * Description: This function simulates the behavior of printf.
+ * Description: this function simulates the behaviour of printf.
  *
- * Return: The number of characters printed or -1 if an error occurs.
+ * Return: the number of characters printed or -1 if an error occurs.
  */
+
 int _printf(const char *format, ...)
 {
-	int outputCount = 0;
+	int outputcount = 0;
 	va_list args;
 
 	if (format == NULL)
@@ -20,48 +21,47 @@ int _printf(const char *format, ...)
 
 	while (*format)
 	{
-		if (*format != '%')
+		if(*format == '%')
 		{
 			putchar(*format);
-			outputCount++;
+			outputcount++;
 		}
 		else
 		{
 			format++;
 			if (*format == '\0')
 				break;
-
 			if (*format == '%')
 				putchar(*format);
 			else if (*format == 'c')
 			{
 				char c = va_arg(args, int);
 				putchar(c);
-				outputCount++;
+				outputcount++;
 			}
 			else if (*format == 's')
 			{
 				char *str = va_arg(args, char *);
-				int strLen = 0;
+				int str_len = 0;
 				int a;
 
-				while (str[strLen] != '\0')
-					strLen++;
+				while(str[str_len] != '\0')
+					str_len++;
 
 				putchar(*str);
-				outputCount++;
+				outputcount++;
 
-				for (a = 1; a < strLen; a++)
+				for (a = 1; a < str_len; a++)
 				{
-					putchar(*(str + a));
-					outputCount++;
+					putchar(*(str+a));
+					outputcount++;
 				}
 			}
 			format++;
 		}
 	}
-
 	va_end(args);
-	return (outputCount);
+	return (outputcount);
 }
+
 
